f0d94eaea45b26fde2a9b4e1863f7f5c697eec21
Paulo Kinopk <pkinopk@gmail.com>
2018-08-11 12:25:22 -0700

    fixed app name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/recipe-book'));
+app.use(express.static(__dirname + '/dist/recipe-list'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/recipe-book/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/recipe-list/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
